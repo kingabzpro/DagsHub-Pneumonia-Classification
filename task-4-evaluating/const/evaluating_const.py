d2cb7a07e8e0645df676aeae7669547f07b6844c
Nir Barazida <nir@dagshub.com>
2021-07-05 08:41:29 +0300

    Add eval stage

--- task-4-evaluating/const/evaluating_const.py
+++ task-4-evaluating/const/evaluating_const.py
@@ -1,13 +1,7 @@
 import os
 
 BASE_PATH = os.getcwd()
-RAW_VAL_PATH = os.path.join(BASE_PATH,'task-2-data-processing/data/processed-data/val')
+PROCESSED_VAL_PATH = os.path.join(BASE_PATH,'task-2-data-processing/data/processed-data/val')
 
-PROD_MODEL_PATH = os.path.join(BASE_PATH,'task-4-evaluation/model/prod_model')
-PRED_PATH = os.path.join(BASE_PATH,'task-4-evaluation/eval/predictions')
-HISTORY_PARAM_PATH = os.path.join(BASE_PATH,'task-4-evaluation/model/TF-Model-Checkpoint/history_params.json')
-CHECKPOINT_PATH = os.path.join(BASE_PATH,'task-4-evaluation/model/TF-Model-Checkpoint/')
-DEL_CHECKPOINT = False
-
-DH_LOG_MET_PATH = os.path.join(BASE_PATH,'task-4-evaluation/eval/metrics/TF_train_final_metrics.csv')
-DH_LOG_PARAM_PATH = os.path.join(BASE_PATH,'task-4-evaluation/eval/params/TF_train_final_params.yml')
+PRED_PATH = os.path.join(BASE_PATH,'task-4-evaluating/eval/predictions')
+DH_LOG_MET_PATH = os.path.join(BASE_PATH,'task-4-evaluating/eval/metrics/metrics.csv')

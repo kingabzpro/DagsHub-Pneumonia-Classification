d563ac4d92a349f4a01307b68b5ed3cf74329bb1
Jinen Setpal <jinens8@gmail.com>
2021-10-06 12:01:32 -0500

    added data loading for chestx dataset

--- task-2-data-processing/src/data_processing.py
+++ task-2-data-processing/src/data_processing.py
@@ -1,56 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+## idg forked from @levanpon1009 on kaggle.com
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from itertools import chain
+from glob import glob
+import pandas as pd
+import numpy as np
 import os
-import glob
-import cv2 as cv
-from src.const.general_const import IMG_SIZE
-from .const.processing_const import RAW_TRAIN_PATH, RAW_TEST_PATH, RAW_VAL_PATH, BASE_PROC_DIR
-from pathlib import Path
-
-# Create list of files
-RAW_TRAIN_PNEUMONIA_FILE_LIST = glob.glob(os.path.join(RAW_TRAIN_PATH,"PNEUMONIA/*"))
-RAW_TRAIN_NORMAL_FILE_LIST = glob.glob(os.path.join(RAW_TRAIN_PATH,"NORMAL/*"))
-
-RAW_TEST_PNEUMONIA_FILE_LIST = glob.glob(os.path.join(RAW_TEST_PATH,"PNEUMONIA/*"))
-RAW_TEST_NORMAL_FILE_LIST = glob.glob(os.path.join(RAW_TEST_PATH,"NORMAL/*"))
-
-RAW_VAL_PNEUMONIA_FILE_LIST = glob.glob(os.path.join(RAW_VAL_PATH,"PNEUMONIA/*"))
-RAW_VAL_NORMAL_FILE_LIST = glob.glob(os.path.join(RAW_VAL_PATH,"NORMAL/*"))
-
-# Load data
-RAW_TRAIN_PNEUMONIA= [cv.resize(cv.imread(path), IMG_SIZE) for path in RAW_TRAIN_PNEUMONIA_FILE_LIST]
-RAW_TRAIN_NORMAL= [cv.resize(cv.imread(path), IMG_SIZE) for path in RAW_TRAIN_NORMAL_FILE_LIST]
-
-RAW_TEST_PNEUMONIA= [cv.resize(cv.imread(path), IMG_SIZE) for path in RAW_TEST_PNEUMONIA_FILE_LIST]
-RAW_TEST_NORMAL= [cv.resize(cv.imread(path), IMG_SIZE) for path in RAW_TEST_NORMAL_FILE_LIST]
-
-RAW_VAL_PNEUMONIA= [cv.resize(cv.imread(path), IMG_SIZE) for path in RAW_VAL_PNEUMONIA_FILE_LIST]
-RAW_VAL_NORMAL= [cv.resize(cv.imread(path), IMG_SIZE) for path in RAW_VAL_NORMAL_FILE_LIST]
-
-# Create path
-Path(BASE_PROC_DIR).mkdir(parents=True, exist_ok=True)
-
-def process_img(img_list_of_list, img_path_list_of_list, BASE_PROC_DIR):
-    """
-    1) Creates base directory with path ../../../task-2-data-processing/data/processed-data/*/**/
-        where:
-         * [train/test/val]
-         ** [NORMAL/PNEUMONIA]
-    2) Save processed images to file with their original name.
-    """
-
-    for IMG_LIST, IMG_PATH_LIST in zip(img_list_of_list,img_path_list_of_list):
-        # (1)
-        base_path_list = [BASE_PROC_DIR] + IMG_PATH_LIST[0].split('/')[-3:-1]
-        Path(os.path.join(*base_path_list)).mkdir(parents=True, exist_ok=True)
-        for img, path in zip(IMG_LIST, IMG_PATH_LIST):
-            # (2)
-            file_path_list = base_path_list + [path.split('/')[-1]]
-            cv.imwrite(os.path.join(*file_path_list), img)
 
 
+CWD = os.getcwd()
+PROJECT_NAME = 'Pneumonia-Classification'
+BATCH_SIZE = 16 # from CheXNet paper 
+DIM = (256, 256)
 
-img_list_of_list = [RAW_TRAIN_PNEUMONIA, RAW_TRAIN_NORMAL, RAW_TEST_PNEUMONIA, RAW_TEST_NORMAL,
-                    RAW_VAL_PNEUMONIA, RAW_VAL_NORMAL]
-img_path_list_of_list = [RAW_TRAIN_PNEUMONIA_FILE_LIST, RAW_TRAIN_NORMAL_FILE_LIST,RAW_TEST_PNEUMONIA_FILE_LIST,
-                         RAW_TEST_NORMAL_FILE_LIST, RAW_VAL_PNEUMONIA_FILE_LIST, RAW_VAL_NORMAL_FILE_LIST]
-# Process the images
-process_img(img_list_of_list, img_path_list_of_list, BASE_PROC_DIR)
+def get_data_generators(CWD = os.getcwd(),
+                        PROJECT_NAME = 'Pneumonia-Classification',
+                        BATCH_SIZE = 16, # from CheXNet paper
+                        DIM = (256, 256)):
+
+    DATA_DIR = CWD[:40] + '/data/'
+
+    df = pd.read_csv(DATA_DIR + 'Data_Entry_2017.csv') # TEST '/home/jinen/git/Pneumonia-Classification/data/
+    df.head()
+
+    data_image_paths = {os.path.basename(x): x for x in glob(os.path.join(DATA_DIR, 'images*', '*', '*.png'))}
+    df['path'] = df['Image Index'].map(data_image_paths.get)
+
+
+    df['Finding Labels'] = df['Finding Labels'].map(lambda x: x.replace('No Finding', ''))
+    labels = np.unique(list(chain(*df['Finding Labels'].map(lambda x: x.split('|')).tolist())))
+    labels = [x for x in labels if len(x) > 0]
+    for label in labels:
+        if len(label) > 1:
+            df[label] = df['Finding Labels'].map(lambda finding: 1.0 if label in finding else 0.0)
+    df.head()
+
+    train_df, valid_df = train_test_split(df, test_size=0.20, random_state=2018, stratify=df['Finding Labels'].map(lambda x: x[:4]))
+    train_df['labels'] = train_df.apply(lambda x: x['Finding Labels'].split('|'), axis=1)
+    valid_df['labels'] = valid_df.apply(lambda x: x['Finding Labels'].split('|'), axis=1)
+
+    core_generator = ImageDataGenerator(rescale=1 / 255,
+                                        samplewise_center=True,
+                                        samplewise_std_normalization=True,
+                                        horizontal_flip=True,
+                                        vertical_flip=False,
+                                        height_shift_range=0.05,
+                                        width_shift_range=0.1,
+                                        rotation_range=5,
+                                        shear_range=0.1,
+                                        fill_mode='reflect',
+                                        zoom_range=0.15)
+
+    training_generator = core_generator.flow_from_dataframe(dataframe=train_df,
+                                                    directory=None,
+                                                    x_col='path',
+                                                    y_col=labels,
+                                                    class_mode='raw',
+                                                    batch_size=BATCH_SIZE,
+                                                    classes=labels,
+                                                    target_size=DIM)
+
+    validation_generator = core_generator.flow_from_dataframe(dataframe=valid_df,
+                                                        directory=None,
+                                                        x_col='path',
+                                                        y_col=labels,
+                                                        class_mode='raw',
+                                                        batch_size=BATCH_SIZE,
+                                                        classes=labels,
+                                                        target_size=DIM)
+
+    test_generator = core_generator.flow_from_dataframe(dataframe=valid_df,
+                                                        directory=None,
+                                                        x_col='path',
+                                                        y_col=labels,
+                                                        class_mode='raw',
+                                                        batch_size=BATCH_SIZE,
+                                                        classes=labels,
+                                                        target_size=DIM)
+
+    return training_generator, validation_generator, test_generator
+
+get_data_generators()
